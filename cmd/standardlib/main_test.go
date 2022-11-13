0cdb45bd4ed556e4b88bfb602392e417a0d2380e
xNok <nokwebspace@gmail.com>
2022-11-13 23:11:01 +0100

    fix: add helper tag

--- cmd/standardlib/main_test.go
+++ cmd/standardlib/main_test.go
@@ -12,6 +12,7 @@ import (
 )
 
 func readTestData(t *testing.T, name string) []byte {
+	t.Helper()
 	content, err := os.ReadFile("../../testdata/" + name)
 	if err != nil {
 		t.Errorf("Could not read %v", name)
